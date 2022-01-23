1b6a627924cf0e5068ee23a6e8a5e90cbec9c15a
Dima <1607warrior@mail.ru>
2022-01-23 14:24:31 +0300

    added app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+"use strict"
+
+console.log('arr' + ' - object'); //arr - object (КАНКАТИНАЦИЯ)
+console.log(4 + ' - object'); //4 - object (КАНКАТИНАЦИЯ)
+console.log(4 + +' - object'); //NAN ,поскольку стоит унарный + 
+
+let incr = 10;
+    decr = 10;
+
+// ++incr;//Префиксная форма
+// --decr;//Постфиксная 
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 ==='8');
+
+//&& - Амперсант (оператор "И")
+// || - оператор "или"
+const isChecked = true;
+      isClose = false;
+
+console.log(isChecked || isClose);//true 
+console.log(isChecked || !isClose); //true ; ! - Отрицание , меняет на противоположный (False превращается в TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
